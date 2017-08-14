5145b1849d1e3584908ce82f7ee05b2c70d063be
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-14 23:18:36 +0200

    minor tweaks

--- Root/ttgammaSlimmer.cxx
+++ Root/ttgammaSlimmer.cxx
@@ -56,15 +56,15 @@ int main(int argc, char** argv)
 
   //string inputPath = "/eos/user/c/caudron/TtGamma_ntuples/v009/CR1/";
 
-  string path = "/eos/atlas/user/j/jwsmith/reprocessedNtuples/v009_flattened/CR1/";
-  //string inputPath = "root://eosatlas//eos/atlas/user/j/jwsmith/reprocessedNtuples/v009/QE2_yichen/";
-  //string channels[] ={"mujets"};
-  string channels[] ={"emu","mumu","ee"};
+  //string path = "/eos/atlas/user/j/jwsmith/reprocessedNtuples/v009_flattened/CR1/";
+  string path = "root://eosuser//eos/user/j/jwsmith/reprocessedNtuples/v009/QE2_yichen/";
+  string channels[] ={"ejets","mujets"};
+  //string channels[] ={"emu","mumu","ee"};
   // Where we save to:
   // Remember to make the directory. I.e. mkdir ../SR1 ; cd ../SR1 ; mkdir emu mumu etc
   // I'm just too lazy.
   //string outputPath = "/eos/atlas/user/j/jwsmith/reprocessedNtuples/v007_training/SR1/";
-  string myPath = "/afs/cern.ch/work/j/jwsmith/github/ttgammaSlimmer/SR1/";
+  string myPath = "root://eosatlas//eos/atlas/user/b/bvolkel/ntuples/ttgamma_13TeV/v009/QE2/";
 
   TFile *newfile;
   TFile *oldFile;
@@ -113,13 +113,17 @@ int main(int argc, char** argv)
           cut="selph_index1 >=0 && event_ngoodphotons==1 && event_njets >= 4 && event_nbjets77 >= 1 && abs(ph_mgammalept[selph_index1] - 91188) > 5000 && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1]";
           // QE2 cuts
           //cut=" ((ejets_2015 && (HLT_e24_lhmedium_L1EM20VH || HLT_e60_lhmedium || HLT_e120_lhloose)) || (ejets_2016 && ((HLT_e26_lhtight_nod0_ivarloose && el_pt[0] < 61000.) || ((HLT_e60_lhmedium_nod0 || HLT_e140_lhloose_nod0) && el_pt[0] > 61000)))) && (selph_index1 >=0 && event_ngoodphotons==1 && event_njets >= 4 && event_nbjets77 >= 1 && abs(ph_mgammalept[selph_index1] - 91188) > 5000 && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1])";
+          // QE2 cuts Benedikt
+          cut=" ((ejets_2015 && (HLT_e24_lhmedium_L1EM20VH || HLT_e60_lhmedium || HLT_e120_lhloose)) || (ejets_2016 && ((HLT_e26_lhtight_nod0_ivarloose && el_pt[0] < 61000.) || ((HLT_e60_lhmedium_nod0 || HLT_e140_lhloose_nod0) && el_pt[0] > 61000))))";
 
         }
         if (c.find("mujets") != std::string::npos) {
           // Direct SR cuts
-          cut="selph_index1 >=0 && event_ngoodphotons==1 && event_njets >= 4 && event_nbjets77 >= 1 && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1]";
+          //cut="selph_index1 >=0 && event_ngoodphotons==1 && event_njets >= 4 && event_nbjets77 >= 1 && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1]";
           // QE2 cuts
           //cut="((mujets_2015 && (HLT_mu20_iloose_L1MU15 || HLT_mu50)) || (mujets_2016 && ((HLT_mu24 && mu_pt[0] < 51000.) || (HLT_mu50 && mu_pt[0] > 51000.)))) && (selph_index1 >=0 && event_ngoodphotons==1 && event_njets >= 4 && event_nbjets77 >= 1 && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1])";
+          // QE2 cuts Benedikt
+          cut="((mujets_2015 && (HLT_mu20_iloose_L1MU15 || HLT_mu50)) || (mujets_2016 && ((HLT_mu24 && mu_pt[0] < 51000.) || (HLT_mu50 && mu_pt[0] > 51000.))))";
         }
         if (c.find("ee") != std::string::npos) {
           cut="selph_index1 >=0 && event_ngoodphotons == 1 && event_nbjets77 >= 1 && met_met > 30000 && (event_mll < 85000 || event_mll > 95000) && (ph_mgammaleptlept[selph_index1] < 85000 || ph_mgammaleptlept[selph_index1] > 95000) && ph_drlept[selph_index1] > 1.0 && ph_isoFCT[selph_index1]";
